344e535d7c10d993101663ab8049103a6491eaba
Brandi Durham <durhambn@g.cofc.edu>
2018-11-21 15:37:38 -0500

    implemented the getInput method and added comments

--- scripts/340Spider.py
+++ scripts/340Spider.py
@@ -1,5 +1,5 @@
 import os
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import urllib2
 import re
 
@@ -9,6 +9,8 @@ import re
 #return user choice: email or phone number
 #the address or number to seach
 def getInput():
+    site = raw_input("Enter the URL you'd like to search: ")
+    type(site)
     print("1: Email Address")
     print("2: Phone Number")
     num = input("Which would you like to find? ")
@@ -18,12 +20,12 @@ def getInput():
         search = raw_input("Enter the email address you'd like to search for: ")
         type(search)
         #print search
-        return num, search
+        return site, num, search
     elif(num ==2):
         search = raw_input("Enter the phone number you'd like to seach for: ")
         type(search)
         #print search
-        return num, search
+        return site, num, search
     else:
         #print ("Not a valid number ... exiting")
         sys.exit("Not a valid number ... exiting")
@@ -33,8 +35,19 @@ def getInput():
 
 #method used to get the source code from the
 #website entered by user
-#return sourve code
-def callSite():
+#return source code
+def callSite(url):
+    response = urllib2.urlopen(url)
+    pageSource = response.read()
+    print(pageSource)
+    return pageSource
+
+#pass in source code and find all the links on the website
+#return list of all the links in source code
+def getLinks(sourcecode):
+    links = []
+
+
 
 
 #method used to seach website for getInput
@@ -50,6 +63,11 @@ def makeTable():
 
 
 #gets user input and stores in vaiables
-num, search = getInput()
+site, num, search = getInput()
+print("Website to search: " + site)
 print("Type of search: " + str(num))
 print("Searching for: " + search)
+# get the source code of the main site
+pageSource = callSite(site)
+#get all the links from the main site
+links = getLinks(pageSource)
